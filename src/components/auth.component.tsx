9e2ba902449a241036534309d95f3fd9f529a261
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-11-05 14:58:31 +0700

    Improved Authentication Method

--- src/components/auth.component.tsx
+++ src/components/auth.component.tsx
@@ -1,44 +1,147 @@
-import React, { useState, useEffect } from 'react'
-import { initializeApp } from 'firebase/app'
-import { generateToken } from '../lib/functions.component'
-import { GoogleAuthProvider, getAuth, signInWithRedirect, getRedirectResult } from 'firebase/auth'
-import { Button } from '@mui/material'
+import React, { useState, useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { generateToken } from '../lib/functions.component';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { Alert, Slide, Button, Snackbar, SlideProps } from '@mui/material';
+
+type TransitionProps = Omit<SlideProps, 'direction'>;
 
 const About = ({ config, handleCredential }: any) => {
+    initializeApp(config);
+    const whiteListedEmails: string[] | undefined =
+        process.env.REACT_APP_WHITELISTED_EMAIL?.split(', ');
+    const [status, setStatus] = useState<{
+        isError: boolean;
+        message:
+            | null
+            | 'Invalid Credentials'
+            | 'Something Went Wrong. Please Try Again Later.'
+            | 'No Whitelisted Emails are Found.';
+    }>({
+        isError: false,
+        message: 'Invalid Credentials',
+    });
+    const [transition, setTransition] = useState<
+        React.ComponentType<TransitionProps> | undefined
+    >(undefined);
+
     useEffect(() => {
-        initializeApp(config)
-        getRedirectResult(getAuth())
-        .then((result) => {
-            if(result) {
-                if(result.user.email === process.env.REACT_APP_ADMIN_EMAIL) {
-                    generateToken(result.user.email)
-                    handleCredential({
-                        isLoading: false,
-                        loggedIn: true
-                    })
-                }else {
-                    handleCredential({ id: 'isLoading', value: false })
-                    return false
-                }
-            }
-        })
-        .catch(err => console.log(err))
-    }, [])
+        for (let i = 0; i < 79; i++) {
+            const div = document.createElement('div');
+            div.style.opacity = `${Math.random() * (0.075 - 0.025) + 0.025}`;
+            document.querySelector('.backdrop-overlay')?.appendChild(div);
+        }
+    }, []);
 
     return (
-        <div className="bg-white container p-10 rounded-corner">
-            <h3 className="center-align">Loofi Dashboard</h3>
-            <Button
-                variant="outlined"
-                className="mt-10"
-                onClick={() => {
-                    signInWithRedirect(getAuth(), new GoogleAuthProvider())
-                }
-            }>
-                Sign in with Google
-            </Button>
-        </div>
-    )
-}
+        <div>
+            <div className="backdrop-overlay"></div>
+            <div className="backdrop">
+                <div className="acrylic-material"></div>
+                <div className="backdrop-image" id="backdrop-image"></div>
+            </div>
 
-export default About
+            <div className="bg-white container p-10 rounded-corner">
+                <h3 className="center-align mb-10">Welcome Back!</h3>
+                <Button
+                    variant="outlined"
+                    className="mt-10 w-100"
+                    onClick={() => {
+                        signInWithPopup(getAuth(), new GoogleAuthProvider())
+                            .then((result) => {
+                                function Transition(props: TransitionProps) {
+                                    return (
+                                        <Slide {...props} direction="right" />
+                                    );
+                                }
+                                setTransition(() => Transition);
+                                if (result) {
+                                    if (whiteListedEmails)
+                                        whiteListedEmails.forEach(
+                                            (email, index) => {
+                                                if (
+                                                    result.user.email === email
+                                                ) {
+                                                    generateToken(
+                                                        result.user.email
+                                                    );
+                                                    handleCredential({
+                                                        id: 'isLoading',
+                                                        value: false,
+                                                    });
+                                                } else if (
+                                                    index ===
+                                                    whiteListedEmails.length - 1
+                                                ) {
+                                                    setStatus({
+                                                        isError: true,
+                                                        message:
+                                                            'Invalid Credentials',
+                                                    });
+                                                    handleCredential({
+                                                        id: 'isLoading',
+                                                        value: false,
+                                                    });
+                                                }
+                                            }
+                                        );
+                                    else {
+                                        setStatus({
+                                            isError: true,
+                                            message:
+                                                'No Whitelisted Emails are Found.',
+                                        });
+                                        setTimeout(
+                                            () =>
+                                                setStatus({
+                                                    isError: false,
+                                                    message: null,
+                                                }),
+                                            5000
+                                        );
+                                    }
+                                } else {
+                                    setStatus({
+                                        isError: true,
+                                        message:
+                                            'Something Went Wrong. Please Try Again Later.',
+                                    });
+                                    setTimeout(
+                                        () =>
+                                            setStatus({
+                                                isError: false,
+                                                message: null,
+                                            }),
+                                        5000
+                                    );
+                                }
+                            })
+                            .catch(() => {
+                                setStatus({
+                                    isError: true,
+                                    message:
+                                        'Something Went Wrong. Please Try Again Later.',
+                                });
+                                setTimeout(
+                                    () =>
+                                        setStatus({
+                                            isError: false,
+                                            message: null,
+                                        }),
+                                    5000
+                                );
+                            });
+                    }}
+                >
+                    Sign in with Google
+                </Button>
+            </div>
+
+            <Snackbar open={status.isError} TransitionComponent={transition}>
+                <Alert severity="error">{status.message}</Alert>
+            </Snackbar>
+        </div>
+    );
+};
+
+export default About;
